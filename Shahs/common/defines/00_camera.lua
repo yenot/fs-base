516c6d90e2f7ff29594defd828997f1c6bfe4d3e
yenot <dmstfilatov@gmail.com>
2015-07-23 01:26:05 +0400

    First upload
    Status as of 23/07/2015

---
+++ Shahs/common/defines/00_camera.lua
@@ -0,0 +1,9 @@
+-- Camera settings:
+--
+NDefines.NFrontend.CAMERA_END_X = 2790.0
+NDefines.NFrontend.CAMERA_END_Y = 250.0
+NDefines.NFrontend.CAMERA_END_Z = 300.0
+NDefines.NFrontend.FRONTEND_POS_X = 490.0
+NDefines.NFrontend.FRONTEND_POS_Y = 250.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1400.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 2790.0
